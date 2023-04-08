f2fc43e470180d50df8f071de7e7476927f673b8
Tommyt-23 <tom@ewok.co.uk>
2023-04-08 10:47:03 +0100

    get the table to actually work

--- leaderboard_website/control.js
+++ leaderboard_website/control.js
@@ -1,19 +1,41 @@
 let csvContentsPromise = fetch('scores.csv')
   .then(response => response.text())
+  .then(contents => {
+    csvContents = contents;
+    console.log(csvContents);
+    // You can use csvContents here or pass it to another function
+    useCsvContents(csvContents);
+  })
   .catch(error => {
     console.error(error);
   });
 
 // Define a function that uses the csvContents variable
 function useCsvContents(csvContents) {
-  
-  // Use csvContents here
+  console.log(csvContents);
+  let rows = csvContents.split("\n");
+  let tableBody = document.getElementById("tableBody");
+
+  // Loop through each row and add it to the table
+  for (let i = 0; i < rows.length; i++) {
+    let columns = rows[i].split(",");
+    let newRow = tableBody.insertRow();
+    for (let j = 0; j < (columns.length + 1); j++) {
+      if (j === 0){
+        let newCell = newRow.insertCell();
+        newCell.appendChild(document.createTextNode(i+1))
+      }
+      else{
+        let newCell = newRow.insertCell();
+        newCell.appendChild(document.createTextNode(columns[j-1]));
+      }
+    }
+  }
 }
 
 // Wait for the contents of the CSV file to be available
 csvContentsPromise.then(csvContents => {
+  console.log(csvContents);
   // Assign the contents to a global variable
   window.csvContents = csvContents;
-  // Call the function that uses the csvContents variable
-  useCsvContents(csvContents);
 });
