003cbafb5ba1255bc702be26cb295684398f6279
Tommyt-23 <tom@ewok.co.uk>
2023-04-17 13:11:19 +0100

    add all godot files so can be run in godot

---
+++ godot/Scenes/leaderboard.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	OS.shell_open("http://docs.godotengine.org/en/3.0/classes/class_os.html#class-os-shell-open")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
